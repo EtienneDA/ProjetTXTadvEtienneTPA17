b04b5593d60838264b5bf13981808d77235533d4
Etienne Daoust <youcancallmedan@hotmail.com>
2022-05-08 23:53:42 -0700

    Initial commit

---
+++ app/src/main/java/com/example/projettxtadvetiennetpa17/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.projettxtadvetiennetpa17;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
