547f4f42ee0fd7cb607fcf43024b5a6111192c54
Etienne Daoust <etienne_daoust@hotmail.fr>
2022-05-29 01:22:46 -0700

    sqlite that actually works

--- app/src/main/java/com/example/projettxtadvetiennetpa17/EscapeActivity.java
+++ app/src/main/java/com/example/projettxtadvetiennetpa17/EscapeActivity.java
@@ -5,14 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import modele.ContentM;
+import modele.DbAdapter;
 
 public class EscapeActivity extends AppCompatActivity {
+    private TextView txtStory;
     Intent intentretour;
+    private DbAdapter dbAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_escape);
+        dbAdapter = new DbAdapter(EscapeActivity.this);
+        setWidgets();
+        afficherStory();
+    }
+
+    private void setWidgets() {
+        txtStory = findViewById(R.id.txtStory4);
+    }
+
+    private void afficherStory() {
+        Intent input = getIntent();
+        int progres = input.getIntExtra("progress", 0);
+        ContentM con = dbAdapter.findStory(progres);
+        txtStory.setText(con.getStory());
     }
 
     public void onClickRetour(View view) {
